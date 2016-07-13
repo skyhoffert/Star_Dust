284e27b2a754a9604fcb1606ee7f2a7747e6eef2
skyhoffert <skyhoffert@gmail.com>
2016-07-13 11:33:04 -0400

    Thinking of ways to do the items/inventory/shop

---
+++ etc/ideafiles/items/Items.cs
@@ -0,0 +1,11 @@
+using UnityEngine.UI;
+namespace ItemNamespace {
+	static class GenericItem {
+		public static Image = FindResource("GenericItemLogo.png");
+		public static float cost = 1;
+		public static float healthFlat = 1;
+		public static float powerFlat = 1;
+		public static float healthPercentage = .01;
+		public static float powerPercentage = .01;
+	}
+}
