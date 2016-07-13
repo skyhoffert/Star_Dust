da84bf9d467c24d0a4928d01b625e5d2bad8187a
skyhoffert <skyhoffert@gmail.com>
2016-07-13 12:13:44 -0400

    Added the generic armor item for future testing

--- etc/ideafiles/items/Items.cs
+++ etc/ideafiles/items/Items.cs
@@ -10,39 +10,26 @@ namespace ItemNamespace {
 		public float powerFlat = 0;
 		public float healthPercentage = 0;
 		public float powerPercentage = 0;
+		public float accelFlat = 0;
 
 		protected GenericItem();
-
-		// applies effect to specified ship
-		// on purchase
-		public void OnPurchase(StarShip starship, Ship ship){
-			starship.TakeResources(cost);
-			ship.AddHealthFromItem(healthFlat);
-			ship.AddHealthPercentageFromItem(healthPercentage);
-			ship.AddPowerFromItem(powerFlat);
-			ship.AddPowerPercentageFromItem(powerPercentage);
-		}
-		public void OnSell(StarShip starship, Ship ship){
-			starship.AddResources(sell);
-			ship.RemoveHealthFromItem(healthFlat);
-			ship.RemoveHealthPercentageFromItem(healthPercentage);
-			ship.RemovePowerFromItem(powerFlat);
-			ship.RemovePowerPercentageFromItem(powerPercentage);
-		}
-		// other methods could be used, such as
-		// this could be run every frame, if it needs to check something
-		// public abstract void OnUpdate(Ship ship);
 	}
-
+	// adds more power
 	class ExtraBattery : Item {
-
 		public ExtraBattery() : base() {
+			Image = Resources.Load("ExtraBatteryIcon.png");
 			cost = 100;
 			sell = 50;
-			// adds 
 			powerFlat = 100;
 		}
-
-	
+	}
+	// adds health
+	class ArmorPiece : Item {
+		public ArmorPiece() : base() {
+			Image = Resources.Load("ArmorPieceIcon.png");
+			cost = 75;
+			sell = 40;	
+			healthFlat = 80;
+		}
 	}
 }
